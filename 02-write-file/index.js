52d8c077fbde777fb47a11d0cc1a59782f93452c
pinkpony1106 <juliaegorova221098@gmail.com>
2023-05-05 23:33:51 +0300

    fixed solution of second task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,17 +6,16 @@ const toFile = path.join(__dirname, "input.txt");
 fs.writeFile(toFile, "", (error) => {
   error ? console.log(error.message) : null;
 });
-
 stdout.write("Write some text!");
 const writeSrt = fs.createWriteStream(toFile);
 stdin.on("data", (data) => {
   if (data.toString().trim() == "exit") {
-    process.exit();
+    stdout.write("Good bye");
+    exit();
   } else {
     writeSrt.write(data.toString());
   }
 });
-
 process.on("SIGINT", () => {
   stdout.write("Bye!");
   exit();
