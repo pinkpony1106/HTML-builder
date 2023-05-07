73af5ab4ca2566db2089478cfc66ea22959b8b98
pinkpony1106 <juliaegorova221098@gmail.com>
2023-05-07 22:26:25 +0300

    added solution to task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require("fs");
+const path = require("path");
 
+fs.readdir(
+  path.join(__dirname, "files"),
+  { withFileTypes: true },
+  (error, files) => {
+    if (error) console.log(error.message);
+    fs.mkdir(
+      path.join(__dirname, "files-copy"),
+      { recursive: true, force: true },
+      (error) => {
+        if (error) console.log(error.message);
+        files.forEach((f) => {
+          fs.copyFile(
+            path.join(__dirname, "files", f.name),
+            path.join(__dirname, "files-copy", f.name),
+            (error) => {
+              if (error) console.log(error.message);
+            }
+          );
+        });
+      }
+    );
+  }
+);
